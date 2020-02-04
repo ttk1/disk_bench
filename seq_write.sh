49ff9ea7aeec24f81ffdad43a06d0234b733503c
tama <tama@ttk1.net>
2020-02-05 01:04:54 +0900

    ddの出力をそのまま使うようにした。

--- seq_write.sh
+++ seq_write.sh
@@ -15,9 +15,7 @@ function write_bench() {
     mkdir bench
   fi
   for ((i=0;i<TEST_COUNT;i++)); do
-    (time -p dd if=/dev/zero of="bench/$i" bs=1M count="$TEST_SIZE" 2>&1) 2>&1 > /dev/null |
-    head -1 |
-    awk '{print '"$TEST_SIZE"' / ($2 + 0.01) " MB/s"}'
+    dd if=/dev/zero of="bench/$i" bs=1M count="$TEST_SIZE" 2>&1 | tail -1
   done
   rm bench/*
 }
